76ffb37adf15b0ee90c40eb24de73a7ab0226401
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-09-10 18:52:13 +0200

    Add Rectangle class

---
+++ src/utils/Rectangle.ts
@@ -0,0 +1,35 @@
+import Point from "./Point";
+
+export default class Rectangle {
+  private _topLeft: Point = new Point(0, 0);
+  private _bottomRight: Point = new Point(0, 0);
+  
+  constructor(topLeftPoint: Point, bottomRightPoint: Point) {
+    this.topLeft = topLeftPoint;
+    this.bottomRight = bottomRightPoint;
+  }
+
+  get topLeft() {
+    return this._topLeft;
+  }
+
+  get bottomRight() {
+    return this._bottomRight;
+  }
+
+  set topLeft(point: Point) {
+    this.validatePoint(point);
+    this._topLeft = point;
+  }
+
+  set bottomRight(point: Point) {
+    this.validatePoint(point);
+    this._bottomRight = point;
+  }
+
+  private validatePoint(point: Point): void {
+    if(!(point instanceof Point)) {
+      throw new TypeError("argument not of type Point");
+    }
+  }
+}
