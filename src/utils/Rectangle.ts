b9d2f13690e5e69fe715aa031b2e44408b4f56d7
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-09-15 14:36:06 +0200

    Add width and height getters to Rectangle

--- src/utils/Rectangle.ts
+++ src/utils/Rectangle.ts
@@ -27,6 +27,14 @@ export default class Rectangle {
     this._bottomRight = point;
   }
 
+  get width() {
+    return this.bottomRight.x - this.topLeft.x;
+  }
+
+  get height() {
+    return this.bottomRight.y - this.topLeft.y;
+  }
+
   private validatePoint(point: Point): void {
     if(!(point instanceof Point)) {
       throw new TypeError("argument not of type Point");
