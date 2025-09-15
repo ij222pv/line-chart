aea76215dfcdd665253924d40aadfc0560f5fde2
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-09-15 14:46:12 +0200

    Fit line in graph area

--- src/renderers/LineChartRenderer.ts
+++ src/renderers/LineChartRenderer.ts
@@ -1,4 +1,7 @@
 import type LineChartState from "../states/LineChartState";
+import Point from "../utils/Point";
+import Rectangle from "../utils/Rectangle";
+import RectangleMapper from "../utils/RectangleMapper";
 import type Renderer from "./Renderer";
 
 export default class LineChartRenderer implements Renderer {
@@ -6,9 +9,14 @@ export default class LineChartRenderer implements Renderer {
     renderingContext.rect(0, 0, chartState.pixelWidth, chartState.pixelHeight);
     renderingContext.stroke();
 
+    const boundary: Rectangle = chartState.getBoundary();
+
+    const rectangleMapper = new RectangleMapper(boundary, new Rectangle(new Point(0, 0), new Point(chartState.pixelWidth, chartState.pixelHeight)));
+
     renderingContext.beginPath();
     for(const point of chartState.points) {
-      renderingContext.lineTo(point.x, point.y);
+      const mappedPoint = rectangleMapper.map(point);
+      renderingContext.lineTo(mappedPoint.x, mappedPoint.y);
     }
     renderingContext.stroke();
   }
