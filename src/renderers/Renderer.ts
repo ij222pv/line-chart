92e70b30fa4e207277950f3ad8c9e6d4ea0834b4
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-09-24 23:03:35 +0200

    Fix Renderer interface to match LineChartRenderer

--- src/renderers/Renderer.ts
+++ src/renderers/Renderer.ts
@@ -1,11 +1,3 @@
-import type ChartState from "../states/ChartState";
-
 export default interface Renderer {
-  /**
-   * Render the given chart state to the given rendering context.
-   */
-  render(
-    renderingContext: CanvasRenderingContext2D,
-    chartState: ChartState,
-  ): void;
+  render(): void;
 }
